deb7973c17ec14e0fcb0dbde8d87ada5ea3479ec
brunoromao01 <bruno-romao@hotmail.com>
2024-05-01 11:09:19 -0300

    base do app

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+
+function App(): React.JSX.Element {
+
+  return (
+    <View style={styles.container}>
+      <Text>
+        OnlyAcademy
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+});
+
+export default App;
